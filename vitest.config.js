aab5a6e72ae2f91a077e2465df7a22e2335d2d50
Rosano Coutinho <github.com@xeari.com>
2026-01-27 09:19:44 +0100

    hide skipped tests

--- vitest.config.js
+++ vitest.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig(({ mode }) => ({
   test: {
+    hideSkippedTests: true,
     // mode defines what ".env.{mode}" file to choose if exists
     env: loadEnv(mode, process.cwd(), ''),
   },
