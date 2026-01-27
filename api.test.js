5160bfce5ffd5bf4098f12bc2006d254b21ab605
Rosano Coutinho <github.com@xeari.com>
2026-01-27 08:24:15 +0100

    spec unauthorized

--- api.test.js
+++ api.test.js
@@ -3,7 +3,7 @@ import util from './util.js'
 
 process.env.SERVER_URL.split(',').forEach(server => {
 
-	describe(server, () => {
+	describe(new URL(server).host, () => {
 
 		const State = {
 			server,
@@ -11,6 +11,31 @@ process.env.SERVER_URL.split(',').forEach(server => {
 			scope: process.env.TOKEN_SCOPE || 'api-test-suite',
 			token_rw: process.env.TOKEN_READ_WRITE,
 		};
+
+		beforeAll(async () => {
+			State.webfinger = await util.webfinger(State.server, State.account);
+			State.baseURL = State.webfinger.href;
+			State.storage = util.storage(State);
+		});
+
+		describe('unauthorized', () => {
+
+			it('handles GET', async () => {
+				const response = await State.storage.get(Math.random().toString());
+				expect(response.status).toBe(401);
+			});
+
+			it('handles PUT', async () => {
+				const response = await State.storage.put(Math.random().toString());
+				expect(response.status).toBe(401);
+			});
+
+			it('handles DELETE', async () => {
+				const response = await State.storage.delete(Math.random().toString());
+				expect(response.status).toBe(401);
+			});
+
+		});
 	});
 
 });
--- util.js
+++ util.js
@@ -10,6 +10,33 @@ const util = {
     return json.links.filter(e => e.rel === 'remotestorage').shift();
   },
 
+  _url: (State, path) => [
+    State.baseURL,
+    State.scope,
+    path,
+  ].join('/'),
+
+  storage (State) {
+    const headers = {
+      'Content-Type': 'application/json',
+    };
+
+    return {
+      get: path => fetch(util._url(State, path), {
+        headers: headers,
+      }),
+      put: (path, body) => fetch(util._url(State, path), {
+        headers: headers,
+        method: 'PUT',
+        body: JSON.stringify(body),
+      }),
+      delete: path => fetch(util._url(State, path), {
+        headers: headers,
+        method: 'DELETE',
+      }),
+    };
+  },
+
 };
 
 export default util;
