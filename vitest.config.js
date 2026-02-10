890ff2cce17dfa694480f0603526736df10f4a8c
Rosano Coutinho <github.com@xeari.com>
2026-02-10 10:33:29 +0100

    purge vitest config

--- vitest.config.js
+++
@@ -1,9 +0,0 @@
-import { loadEnv } from 'vite'
-import { defineConfig } from 'vitest/config'
-
-export default defineConfig(() => ({
-  test: {
-    hideSkippedTests: true,
-    env: loadEnv(process.env.NODE_ENV, process.cwd(), ''),
-  },
-}));
